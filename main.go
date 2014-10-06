268ef9c4e7c8d49d4c0af5a8e4b59df3367c73e5
Jack Chen (chendo) <github@chen.do>
2014-10-07 00:19:51 +1100

    Allow specifying of domain to proxy

--- main.go
+++ main.go
@@ -1,26 +1,29 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/SlyMarbo/spdy"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
-	"strings"
 	"time"
 )
 
+var (
+	domain string
+)
+
 func handler(rw http.ResponseWriter, req *http.Request) {
-	req.Host = strings.Replace(req.Host, "au.", "", 1)
-	req.Host = strings.Replace(req.Host, ":44300", "", 1)
-	req.URL, _ = url.Parse(fmt.Sprintf("https://%s%s", req.Host, req.RequestURI))
+	req.URL, _ = url.Parse(fmt.Sprintf("https://%s%s", domain, req.RequestURI))
 	req.RequestURI = ""
 
 	start := time.Now()
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		fmt.Printf("Error: %s\n", err)
+		log.Printf("%4s %s: Error: %s", req.Method, req.URL.String(), err)
+		return
 	}
 
 	for k, vs := range resp.Header {
@@ -31,12 +34,16 @@ func handler(rw http.ResponseWriter, req *http.Request) {
 	rw.WriteHeader(resp.StatusCode)
 	body, _ := ioutil.ReadAll(resp.Body)
 	rw.Write(body)
-	fmt.Printf("%4s %s: %f\n", req.Method, req.URL.String(), time.Now().Sub(start).Seconds())
+	log.Printf("%4s %s: %fms\n", req.Method, req.URL.String(), time.Now().Sub(start).Seconds()*1000)
 }
 
 func main() {
+	flag.StringVar(&domain, "domain", "", "domain to proxy")
+	bind := flag.String("bind", ":44300", "bind to")
+	flag.Parse()
 	http.HandleFunc("/", handler)
-	err := spdy.ListenAndServeTLS(":44300", "server.crt", "server.key", nil)
+	log.Printf("Proxing to %s on %s", domain, *bind)
+	err := spdy.ListenAndServeTLS(*bind, "server.crt", "server.key", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
